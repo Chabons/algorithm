ab569c17e13a23219ea9cd9ae39a825ab10fbb5e
xcy <david.xcy55@gmail.com>
2021-06-15 21:03:00 +0800

    LFU

--- cache_expire/LFU.cpp
+++ cache_expire/LFU.cpp
@@ -11,7 +11,54 @@ public:
     int get(int key) {
         if (m_cap <= 0 || m_caches.empty()) return -1;
 
-        auto it = m_caches.find(key);
+        auto it = m_cache_maps.find(key);
+        if (it == m_cache_maps.end()) return -1;
+
+        Node n = *(it->second);
+        ++n.count;
+        n.time = ++m_time;
+        m_caches.erase(it->second);
+        auto ret = m_caches.emplace(std::move(n));
+        if (!ret.second) return -1;
+
+        it->second = ret.first;
+
+        return n.value;
+    }
+
+    void set(int k, int v) {
+        if (m_cap <= 0) return;
+
+        auto it = m_cache_maps.find(k);
+        if (it == m_cache_maps.end()) {
+            if (m_cap <= m_caches.size())
+            {
+                m_cache_maps.erase(m_caches.begin()->key);
+                m_caches.erase(m_caches.begin());
+            }
+
+            auto ret = m_caches.emplace(k, v, 1, ++m_time);
+            if (!ret.second) return;
+
+            m_cache_maps[k] = ret.first;
+        } else {
+            Node n = *(it->second);
+            n.value = v;
+            ++n.count;
+            n.time = ++m_time;
+            m_caches.erase(it->second);
+
+            auto ret = m_caches.emplace(std::move(n));
+            if (!ret.second) return;
+
+            it->second = ret.first;
+        }
+    }
+
+    void print() {
+        std::cout << "-----------------begin-----------------" << std::endl;
+        std::for_each(m_caches.begin(), m_caches.end(), [](const Node& n) { printf("Node(%d->%d|%d|%d)\n", n.key, n.value, n.count, n.time);});
+        std::cout << "-----------------end-----------------" << std::endl;
     }
 
 private:
@@ -22,19 +69,37 @@ private:
         int time;
         Node() : key(0), value(0), count(0), time(0) {}
         Node(int key, int value, int cnt, int time) : key(key), value(value), count(cnt), time(time) {}
+
+        bool operator<(const Node& other) const {
+            return count == other.count ? time < other.time : count < other.count;
+        }
     };
 
-    typedef std::unordered_map<int, Node> CacheMap;
-    auto CacheCompare = [](const CacheMap::const_iterator& lv, const CacheMap::const_iterator& rv) -> bool { return lv->count == rv->count ? lv->time < rv->time : lv->count < rv->count;};
-    typedef std::set<CacheMap::const_iterator, decltype(CacheCompare)> CacheSet(CacheCompare);
+    typedef std::set<Node> CacheSet;
+    typedef std::unordered_map<int, CacheSet::const_iterator> CacheMap;
 
     uint32_t m_cap;
     uint32_t m_time;
-    CacheMap m_caches;
-    CacheSet m_cache_set;
+    CacheSet m_caches;
+    CacheMap m_cache_maps;
 };
 
 int main(int argc, const char **argv) {
+    LFUCache test(5);
+    test.set(1, 1);
+    test.set(2, 2);
+    test.set(3,3 );
+    test.set(4,4);
+    test.set(5,5);
+    test.print();
+    test.set(6,6);
+    test.print();
+    test.set(3,10);
+    test.print();
+    test.get(2);
+    test.print();
+    test.set(7,7);
+    test.print();
 
     return 0;
 }
